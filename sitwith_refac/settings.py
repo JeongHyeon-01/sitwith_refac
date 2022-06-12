f2e9b4a260bf300b004611c57e69b96c5d32486f
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-13 08:13:00 +0900

    fix

--- sitwith_refac/settings.py
+++ sitwith_refac/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.coom']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
