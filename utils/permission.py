27e9b3015ba3769c84e974414386a7dec446b278
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-12 22:56:27 +0900

    add permission

--- utils/permission.py
+++ utils/permission.py
@@ -13,3 +13,10 @@ class IsAdminUser(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user and request.user.is_staff
+
+
+class IsAdminUserOrReadOnly(permissions.IsAdminUser):
+    
+    def has_permission(self, request, view):
+        is_admin = super().has_permission(request, view)
+        return request.method in permissions.SAFE_METHODS or is_admin
