32e869c5d958f389b1d621f38e16286c0cf05086
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-15 14:16:36 +0900

    fix

--- sitwith_refac/settings.py
+++ sitwith_refac/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = SECRET_KEY
-SECRET_KEY = os.environ['secret_key']
+
+SECRET_KEY = os.environ.get['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
