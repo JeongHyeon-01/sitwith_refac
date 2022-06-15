78f159819a3fd068f5a59a194ed8e33854b2f1b1
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-15 14:02:03 +0900

    fix

--- sitwith_refac/settings.py
+++ sitwith_refac/settings.py
@@ -63,7 +63,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
