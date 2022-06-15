985eb89c026be44c2248dbbb59476e0c4ae8b508
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-15 13:55:11 +0900

    fix

--- sitwith_refac/settings.py
+++ sitwith_refac/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -203,6 +203,3 @@ SIMPLE_JWT = {
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
